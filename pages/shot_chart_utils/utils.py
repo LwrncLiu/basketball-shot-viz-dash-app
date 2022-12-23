b92f13e7bb297662ab83cf9b611692009ba2eb39
Lawrence Liu <lawrence.liu1@accesscfa.com>
2022-12-23 12:43:03 -0600

    remove remananents of multi-page app

--- pages/shot_chart_utils/utils.py
+++
@@ -1,9 +0,0 @@
-def calculate_quadratic_values(a, b, c):
-    '''
-    Given values a, b, and c,
-    the function returns the output of the quadratic formula
-    '''
-    x1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
-    x2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
-
-    return x1, x2
